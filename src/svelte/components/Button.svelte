7c3f645911542badc2060bf57ebd4d905ce1ba56
shiki <kishintorii@gmail.com>
2024-09-24 22:53:44 +0900

    save

---
+++ src/svelte/components/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let onClick: () => void = () => {};
+</script>
+
+<button
+    class="w-fit h-fit p-2 bg-sky-500 rounded-full shadow-lg shadow-sky-500/40 transition-shadow hover:shadow-sky-500/60"
+    on:click={onClick}
+    {...$$props}
+>
+    <slot />
+</button>
---
+++ src/svelte/components/Dialog.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import Icon from '@iconify/svelte'
+</script>
+
+<div class="relative w-screen h-screen {$$props.class}" {...$$props}>
+    <div class="fixed inset-0 z-50 flex items-center justify-center">
+        <div class="relative z-10 bg-white p-4">
+            <slot />
+        </div>
+    </div>
+    <div class="fixed">
+        <button class="fixed inset-0 z-0 w-screen h-screen bg-black opacity-50">
+            <Icon icon="akar-icons:close" class="absolute top-4 right-4 w-8 h-8 text-white" />
+        </button>
+    </div>
+</div>
